3c49b0ff316602db45d8efcb743b3ba635242651
catherinev56 <51975176+catherinev56@users.noreply.github.com>
2019-06-20 09:56:04 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+function sumNumbers(){
+var number_one = document.getElementById("numberOne").value
+
+var number_two = document.getElementById("numberTwo").value
+
+var answer = (parseInt(number_one) + parseInt(number_two))
+
+var showAnswer = document.getElementById("sum").innerHTML = answer
+}
+function subtractNumbers(){
+var number_one = document.getElementById("numberOne").value
+
+var number_two = document.getElementById("numberTwo").value
+
+var answer = (parseInt(number_one) - parseInt(number_two))
+
+var showAnswer = document.getElementById("subtract").innerHTML = answer
+}
+function divideNumbers(){
+var number_one = document.getElementById("numberOne").value
+
+var number_two = document.getElementById("numberTwo").value
+
+var answer = (parseInt(number_one) / parseInt(number_two))
+
+var showAnswer = document.getElementById("divide").innerHTML = answer
+}
+function multiplyNumbers(){
+var number_one = document.getElementById("numberOne").value
+
+var number_two = document.getElementById("numberTwo").value
+
+var answer = (parseInt(number_one) * parseInt(number_two))
+
+var showAnswer = document.getElementById("multiply").innerHTML = answer
+}
